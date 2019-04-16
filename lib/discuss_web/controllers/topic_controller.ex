2092e9d2a82af2bc72452c752f53df1403552a04
IlyaOsotov <soten58@yandex.ru>
2019-04-16 21:44:16 +0300

    added create topics

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -7,4 +7,15 @@ defmodule DiscussWeb.TopicController do
 
     render(conn, "new.html", changeset: changeset)
   end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+        IO.inspect(changeset)
+    end
+  end
 end
