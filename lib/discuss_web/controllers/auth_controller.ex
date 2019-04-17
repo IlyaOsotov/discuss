8e34015d05080b8dd8650ef8713968a7467d06b6
IlyaOsotov <soten58@yandex.ru>
2019-04-17 22:13:50 +0300

    implemented signin logic

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -2,7 +2,42 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    
+  alias Discuss.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      nickname: auth.info.email,
+      avatar: auth.info.urls.avatar_url,
+      provider: "github"
+    }
+
+    changeset = User.changeset(%User{}, user_params)
+
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_get_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Signing in successful!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Something went wrong when signing in")
+        |> redirect(to: Routes.topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_get_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 end
