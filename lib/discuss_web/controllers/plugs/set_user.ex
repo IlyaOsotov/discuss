5af1cd161b842c7f9abd4ca9f113d4252df76b39
IlyaOsotov <soten58@yandex.ru>
2019-04-17 22:51:57 +0300

    typo fix

--- lib/discuss_web/controllers/plugs/set_user.ex
+++ lib/discuss_web/controllers/plugs/set_user.ex
@@ -10,7 +10,7 @@ defmodule Discuss.Plugs.SetUser do
   end
 
   def call(conn, _params) do
-    user_id = get_sessioin(conn, :user_id)
+    user_id = get_session(conn, :user_id)
 
     cond do
       user = user_id && Repo.get(User, user_id) ->
