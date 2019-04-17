bc5f0e0ec1ed719b96fea4a637528bca748e9ff8
IlyaOsotov <soten58@yandex.ru>
2019-04-17 20:55:50 +0300

    added user model

---
+++ lib/discuss/user.ex
@@ -0,0 +1,21 @@
+defmodule Discuss.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Discuss.User
+
+  schema "users" do
+    field :email, :string
+    field :provider, :string
+    field :token, :string
+    field :nickname, :string
+    field :avatar, :string
+
+    timestamps()
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email, :provider, :token, :nickname, :avatar])
+    |> validate_required([:email, :provider, :token, :nickname, :avatar])
+  end
+end
